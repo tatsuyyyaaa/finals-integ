46b4cc1fcbd9a66d872b47acce2facb1c1223dd4
tatsuyyyaaa <mmanansala266@gmail.com>
2025-07-21 03:25:25 +0800

    updated login

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
+// plugins/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-}
+  apiKey: "AIzaSyD27B8oNF6xVvc0U1FmTWjvonovaPWrD1o",
+  authDomain: "bgremover-43c03.firebaseapp.com",
+  projectId: "bgremover-43c03",
+  storageBucket: "bgremover-43c03.appspot.com",
+  messagingSenderId: "317754869217",
+  appId: "1:317754869217:web:ae97e3281aa468b0513ea0",
+  measurementId: "G-2WNFXT7RXL"
+};
 
-const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-setPersistence(auth, browserLocalPersistence)
-
-export { auth }
+// Export Firebase services
+export const auth = getAuth(app);
